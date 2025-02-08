d48f181b860afeac9dab6869592a8cc146caf8b9
mayonnayz <mayozyann@gmail.com>
2025-02-08 20:25:05 +0800

    Organized files

---
+++ FA1/packages.R
@@ -0,0 +1,5 @@
+tinytex::install_tinytex()
+install.packages("rmarkdown")
+install.packages("e1071")
+install.packages("here")
+library(here)
--- packages.R
+++
@@ -1,5 +0,0 @@
-tinytex::install_tinytex()
-install.packages("rmarkdown")
-install.packages("e1071")
-install.packages("here")
-library(here)
